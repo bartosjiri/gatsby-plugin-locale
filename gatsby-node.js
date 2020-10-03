7a99c116046db9de404e75e978c1ba9339a0d7ac
bartosjiri <me@bartosjiri.com>
2020-10-03 16:17:11 +0200

    Update file loaders

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
 /* eslint-disable global-require */
-/* eslint-disable import/no-dynamic-require */
-const fs = require('fs')
+const fs = require('fs').promises
 const fg = require('fast-glob')
 const matter = require('gray-matter')
 
@@ -10,67 +9,85 @@ exports.createPages = async ({actions, reporter}, pluginOptions) => {
     defaultLocale, generalFolder, pagesFolder, logGeneratedPages
   } = pluginOptions
 
-  let availableLocales = {
-    locales: {},
-    pages: {}
-  }
-
   // Normalize folder paths
   const normGeneralFolder = generalFolder.replace(/\\/g, '/').replace(/\/+$/, '')
   const normPagesFolder = pagesFolder.replace(/\\/g, '/').replace(/\/+$/, '')
 
   // Import general locales
-  const generalFiles = await fg([`${normGeneralFolder}/*.json`])
-  if (generalFiles.length > 0) {
-    generalFiles.forEach(filePath => {
-      const fileData = require(filePath)
-      // eslint-disable-next-line no-useless-escape
-      const localeRegex = `(${normGeneralFolder.replace('/', '\/')}\/)|(.json)`
-      const locale = filePath.replace(new RegExp(localeRegex, 'g'), '')
+  const populateGeneralLocales = async () => {
+    const generalFiles = await fg([`${normGeneralFolder}/*.json`])
 
-      availableLocales = {
-        ...availableLocales,
-        locales: {
-          ...availableLocales.locales,
+    let generalLocales = {}
+
+    if (generalFiles.length > 0) {
+      generalFiles.forEach(filePath => {
+        const fileData = require(filePath)
+        // eslint-disable-next-line no-useless-escape
+        const localeRegex = `(${normGeneralFolder.replace('/', '\/')}\/)|(.json)`
+        const locale = filePath.replace(new RegExp(localeRegex, 'g'), '')
+
+        generalLocales = {
+          ...generalLocales,
           [locale]: {
             ...fileData
           }
         }
-      }
-    })
+      })
+    }
+
+    return generalLocales
   }
 
   // Import page locales
-  const pagesFiles = await fg([`${normPagesFolder}/**/*.md`])
-  if (pagesFiles.length > 0) {
-    pagesFiles.forEach(filePath => {
-      let fileData = ''
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-          reporter.panicOnBuild(`[gatsby-plugin-locales] Error while reading file "${filePath}".`)
-        }
-        fileData = data
-      })
-      // eslint-disable-next-line no-useless-escape
-      const pageRegex = `(${normPagesFolder.replace('/', '\/')}\/)|(.md)`
-      const pagePath = filePath.replace(new RegExp(pageRegex, 'g'), '')
-      const page = pagePath.substring(0, pagePath.lastIndexOf('/'))
-      const locale = pagePath.substring(pagePath.lastIndexOf('/') + 1, pagePath.length)
+  const populatePagesLocales = async () => {
+    const pagesFiles = await fg([`${normPagesFolder}/**/*.md`])
 
-      const parsedFileData = matter(fileData)
+    let pagesLocales = {}
 
-      availableLocales = {
-        ...availableLocales,
-        pages: {
-          ...availableLocales.pages,
+    if (pagesFiles.length > 0) {
+      for (const filePath of pagesFiles) {
+        const fileData = await fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            reporter.panicOnBuild(`[gatsby-plugin-locales] Error while reading file "${filePath}".`)
+          }
+          return data
+        })
+
+        // eslint-disable-next-line no-useless-escape
+        const pageRegex = `(${normPagesFolder.replace('/', '\/')}\/)|(.md)`
+        const pagePath = filePath.replace(new RegExp(pageRegex, 'g'), '')
+        const page = pagePath.substring(0, pagePath.lastIndexOf('/'))
+        const locale = pagePath.substring(pagePath.lastIndexOf('/') + 1, pagePath.length)
+
+        const parsedFileData = matter(fileData)
+
+        pagesLocales = {
+          ...pagesLocales,
           [page]: {
-            ...availableLocales.pages[page],
+            ...pagesLocales[page],
             [locale]: {
-              frontmatter: {...parsedFileData.data}
+              frontmatter: {
+                ...parsedFileData.data
+              }
             }
           }
         }
       }
-    })
+    }
+
+    return pagesLocales
   }
+
+  // Compile locales
+  const compileLocales = async (generalLocales, pagesLocales) => ({
+    locales: {...generalLocales},
+    pages: {...pagesLocales}
+  })
+
+  const generalLocales = await populateGeneralLocales()
+  const pagesLocales = await populatePagesLocales()
+  const availableLocales = await compileLocales(generalLocales, pagesLocales)
+
+  // @DEBUG:
+  console.log('availableLocales: ', availableLocales)
 }
