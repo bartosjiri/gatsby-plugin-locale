1ae8f682db5ee3683fb566624af41de23ead055c
bartosjiri <me@bartosjiri.com>
2020-11-16 22:25:16 +0100

    Update context provider & minor updates

--- src/context/LocalesContextProvider.js
+++ src/context/LocalesContextProvider.js
@@ -1,30 +1,24 @@
 import React, {createContext, useReducer} from 'react'
 import PropTypes from 'prop-types'
 
-import config from '../config.json'
-
 export const LocalesStateContext = createContext()
 export const LocalesDispatchContext = createContext()
 
-const initialState = {
-  defaultLocale: config.defaultLocale,
-  locale: config.defaultLocale
-}
-
 const reducer = (state, action) => {
-  switch (action.type) {
-    case 'CHANGE_LOCALE': {
-      return {
-        ...state,
-        locale: action.payload
-      }
-    }
-    default:
-      throw new Error('[LocalesContextProvider] Invalid action type')
-  }
+  return state
 }
 
 const LocalesContextProvider = ({children}) => {
+  const initialState = {
+    location: {
+      pathname: children.props.location.pathname,
+      search: children.props.location.search
+    },
+    locales: {
+      ...children.props.pageContext.locales
+    }
+  }
+
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <LocalesStateContext.Provider value={state}>
